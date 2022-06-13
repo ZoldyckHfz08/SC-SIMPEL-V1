d8214971f0a263aa400d4e17ac52ed50da0ac3a2
ZoldyckHfz08 <107298390+ZoldyckHfz08@users.noreply.github.com>
2022-06-13 20:00:40 +0700

    Update groupowner.js

--- src/groupowner.js
+++ src/groupowner.js
@@ -1,9 +1,9 @@
 exports.groupowner = (prefix) => {
 return `*- LINK GROUP OWNER BOT*
 
-1. https://chat.whatsapp.com/G1PlSE3VR6e99hmMaxzpkg
-2. https://chat.whatsapp.com/LV3DOUhJerlHBkvAjcB5zr
-3. https://chat.whatsapp.com/LNCchYC5Iyp2VLpqy0oK7C
+1. https://chat.whatsapp.com/C3jVIzCxvXrC1PpuZqTs4l
+2. https://chat.whatsapp.com/LIsAWhdJ9AiLbN0Y9M4uo1
+3. https://chat.whatsapp.com/KWyv6tH9yc0GbDqa5TwNR4
 
 _Jika mau bergabung jangan lupa baca deskripsi!!_`
 }
